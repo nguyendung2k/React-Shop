826837625e190dbd9a0a4c2c8c8ecb049fa948cb
nguyendung2k <nguyenbadung2k@gmail.com>
2022-05-04 15:31:44 +0700

    Make Product Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,12 @@ import HeroSlider from '../components/HeroSlider'
 import Section,{SectionTitle,SectionBody} from '../components/Section'
 import PolicyCard from '../components/PolicyCard';
 import Grid from '../components/Grid'
+import ProductCard from '../components/ProductCard';
 
 import heroSliderData from '../assets/fake-data/hero-slider'
 import policy from '../assets/fake-data/policy'
+import productData from '../assets/fake-data/products'
+import banner from '../assets/images/banner.png'
 
 const Home = () => {
   return (
@@ -32,9 +35,9 @@ const Home = () => {
                     gap={20}
                 >
                     {
-                        policy.map((item,index) => <Link to = "/policy">
+                        policy.map((item,index) => <Link key={index} to = "/policy">
                             <PolicyCard
-                                key={index}
+                                
                                 name={item.name}
                                 description ={item.description}
                                 icon={item.icon}
@@ -45,6 +48,107 @@ const Home = () => {
             </SectionBody>
         </Section>
         {/* end policy section */}
+
+        {/* best selling section */}
+            <Section>
+                <SectionTitle>
+                    Top sản phẩm bán chạy trong tuần
+                </SectionTitle>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+                     >   
+                     {
+                         
+                             productData.getProducts(4).map((item,index) => (
+                                 <ProductCard
+                                     key={index}
+                                     img01={item.image01}
+                                     img02={item.image02}
+                                     name={item.title}
+                                     price={Number(item.price)}
+                                     slug={item.slug}
+                                 />
+                             ))
+                         
+                     }
+                    </Grid>
+                </SectionBody>
+            </Section>
+        {/*end best selling section */}
+
+        {/*new arrival section */}
+            <Section>
+                <SectionTitle>
+                    Sản phẩm mới
+                </SectionTitle>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+                     >   
+                        {
+                            
+                            productData.getProducts(8).map((item,index) => (
+                                <ProductCard
+                                    key={index}
+                                    img01={item.image01}
+                                    img02={item.image02}
+                                    name={item.title}
+                                    price={Number(item.price)}
+                                    slug={item.slug}
+                                />
+                            ))
+                            
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+        {/*end new arrival section */}
+
+        {/* banner */  }
+        <Section>
+            <SectionBody>
+                <Link to = "/catalog">
+                    <img src={banner} alt="" />
+                </Link>
+            </SectionBody>
+        </Section>
+        {/* end banner */}
+
+        {/*popular product section */}
+        <Section>
+            <SectionTitle>
+                Phổ biến
+            </SectionTitle>
+            <SectionBody>
+                <Grid
+                    col={4}
+                    mdCol={2}
+                    smCol={1}
+                    gap={20}
+                    >   
+                    {
+                        productData.getProducts(12).map((item,index) => (
+                            <ProductCard
+                                key={index}
+                                img01={item.image01}
+                                img02={item.image02}
+                                name={item.title}
+                                price={Number(item.price)}
+                                slug={item.slug}
+                            />
+                        ))
+                    }
+                </Grid>
+            </SectionBody>
+        </Section>
+        {/*end popular product section */}
     </Helmet>
   );
 };
